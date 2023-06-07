17d887e5c705e43ed8a756b7f1da747b73df52db
Jellu <105118442+Debilesque@users.noreply.github.com>
2023-06-06 21:52:02 -0600

    a prueba de balas

--- proyecto.c
+++ proyecto.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-// TODO: Validar fin del archivo y mandar error si no se ha cerrado
+// TODO: Validar fin del archivo
 // el comentario multilínea
-// TODO: Permir la lectura de otra línea si el autómata terminó con
-// Qf
 // Manejar errores
+
+/* ------ LIMITACIONES ------ */
+/* Un autómata por línea.
+ * No se permiten este tipo de comentarios multilínea.
+ * No acepta acentos 
+ */
 
 void print_string(char *string) {
     int i = 0;
@@ -14,15 +18,15 @@ void print_string(char *string) {
         printf("%c", string[i]);
         i++;
     }
-    printf("\n");
+    /* printf("\n"); */
 }
 
 void print_state(char *string, int i, char state) {
     char character = string[i];
     if (state == 'F') {
-        printf("%c, Q(F)\n", character);
+        printf("%c (%d), Q(F)\n", character, character);
     } else if (state == 'E') {
-        printf("%c, Q(E)\n", character);
+        printf("%c (%d), Q(E)\n", character, character);
     } else {
         printf("%c (%d), Q(%c)\n", character, character, state);
     }
@@ -86,6 +90,7 @@ char statec_0(char character, char state) {
         state = '1';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, no es un comentario: ");
     }
     return state;
@@ -98,6 +103,7 @@ char statec_1(char character, char state) {
         state = '4';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, no es un comentario: ");
     }
     return state;
@@ -111,6 +117,7 @@ char statec_2(char character, char state) {
         state = '3';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida: ");
     }
     return state;
@@ -119,10 +126,12 @@ char statec_2(char character, char state) {
 char statec_3(char character, char state) {
     if (character >= 32 && character <= 126) {
         state = '3';
-    } else if (character == '\n') {
+    } else if (character == '\n' ||
+               character == '\r') {
         state = 'F';
     } else {
         state = 'E';
+        printf("\n");
         printf("Error en el comentario: ");
     }
     return state;
@@ -135,6 +144,7 @@ char statec_4(char character, char state) {
         state = '5';
     } else {
         state = 'E';
+        printf("\n");
         printf(
                "Se coloco un caracter no permitido para el comentario multilinea: ");
     }
@@ -149,6 +159,7 @@ char statec_5(char character, char state) {
         state = '5';
     } else {
         state = 'E';
+        printf("\n");
         printf(
                "Se coloco un caracter no permitido para el comentario multilinea: ");
     }
@@ -160,32 +171,39 @@ char statec_6(char character, char state) {
         state = 'F';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente el comentario multilinea: ");
     }
     return state;
 }
 
 char statec_final(char character, char state) {
-    if (character == '\0' || character == '\n') {
+    if (character == '\0' || character == '\n' ||
+        character == '\r') {
         state = 'F';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se llego al estado final, error de sintaxis: ");
     }
     return state;
 }
 
-char automata_comentarios(char *string) {
+char automata_comentarios(char *string, int number, char prev) {
     
     int i = 0;
     int iter = 0;
 
     char state = '0';
+    if (prev == '5'){
+        printf("Estado previo = %c\n", prev);
+        state = '5';
+    }
+
     char character = string[i];
     
-    printf("Entrada: ");
+    printf("Línea %d: ", number);
     print_string(string);
-    printf("\n");
     
     while (character != '\0') {
         switch (state) {
@@ -216,6 +234,9 @@ char automata_comentarios(char *string) {
         }
         
         print_state(string, i, state);
+        /* if(state == 'E' || state == 'F'){ */
+        /*     print_state(string, i, state); */
+        /* } */
         
         if (state == 'E') {
             break;
@@ -224,14 +245,10 @@ char automata_comentarios(char *string) {
         character = string[i];
     }
     
-    if (state == 'E') {
-        state = i + 48;
-    }
-    
-    if (character == '\0' && state != 'F') {
-        state = '.';
-    }
-    
+    /* if (state == 'E') { */
+    /*     state = i + 48; */
+    /* } */
+
     return state;
 }
 
@@ -252,6 +269,7 @@ char statef_1(char character, char state) {
         state = 'g';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, no es una instrucción cin o cout: ");
     }
     return state;
@@ -262,6 +280,7 @@ char statef_2(char character, char state) {
         state = '3';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, no es una instrucción cout: ");
     }
     return state;
@@ -272,6 +291,7 @@ char statef_3(char character, char state) {
         state = '4';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, no es una instrucción cout: ");
     }
     return state;
@@ -284,6 +304,7 @@ char statef_4(char character, char state) {
         state = '6';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta (<): ");
     }
     return state;
@@ -296,6 +317,7 @@ char statef_5(char character, char state) {
         state = '6';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta (<): ");
     }
     return state;
@@ -306,6 +328,7 @@ char statef_6(char character, char state) {
         state = '7';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta (falta segundo <): ");
     }
     return state;
@@ -325,6 +348,7 @@ char statef_7(char character, char state) {
         state = 'd';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta: ");
     }
     return state;
@@ -341,6 +365,7 @@ char statef_8(char character, char state) {
         state = 'd';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta: ");
     }
     return state;
@@ -355,6 +380,7 @@ char statef_9(char character, char state) {
     
     else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la cadena a imprimir: ");
     }
     return state;
@@ -366,9 +392,9 @@ char statef_a(char character, char state) {
     } else if (character >= 0 && character <= 128) {
         state = 'a';
     }
-    
     else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la cadena a imprimir: ");
     }
     return state;
@@ -380,11 +406,11 @@ char statef_b(char character, char state) {
     } else if (character == 32) {
         state = 'b';
     }
-    
     else if (character == '<') {
         state = '6';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la instrucción o no se agregaron "
                "cadenas/variables adicionales: ");
     }
@@ -394,10 +420,15 @@ char statef_b(char character, char state) {
 char statef_c(char character, char state) {
     if (character == ';') {
         state = 'F';
+    } else if(character == ' '){
+        state = 'c';
+    } else if (character == '<') {
+        state = '6';
     } else if (character >= 48 && character <= 57) {
         state = 'c';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la instrucción o no se agregaron "
                "cadenas/variables adicionales: ");
     }
@@ -407,10 +438,13 @@ char statef_c(char character, char state) {
 char statef_d(char character, char state) {
     if (character == ';') {
         state = 'F';
+    } else if (character == '<') {
+        state = '6';
     } else if (character >= 0 && character <= 128) {
         state = 'e';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la instrucción o se agregaron "
                "cadenas/variables adicionales: ");
     }
@@ -420,10 +454,11 @@ char statef_d(char character, char state) {
 char statef_e(char character, char state) {
     if (character == ';') {
         state = 'F';
-    } else if (character >= 0 && character <= 128) {
+    } else if (character >= 32 && character <= 126) {
         state = 'e';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la instrucción o se agregaron "
                "cadenas/variables adicionales: ");
     }
@@ -437,6 +472,7 @@ char statef_g(char character, char state) {
         state = 'h';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cin incorrecta: ");
     };
     return state;
@@ -450,6 +486,7 @@ char statef_h(char character, char state) {
         state = 'j';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta (> o espacio): ");
     }
     return state;
@@ -463,6 +500,7 @@ char statef_i(char character, char state) {
         state = 'j';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, sintaxis de cout incorrecta (> o espacio): ");
     }
     return state;
@@ -474,6 +512,7 @@ char statef_j(char character, char state) {
         state = 'k';
     } else {
         state = 'E';
+        printf("\n");
         printf(
                "Cadena no valida, sintaxis de cout incorrecta (falta el segundo >): ");
     }
@@ -483,12 +522,18 @@ char statef_j(char character, char state) {
 char statef_k(char character, char state) {
     
     if (character == ' ') {
-        
         state = 'l';
-    } else if (character == '_' || character >= 65 || character <= 90) {
+    } else if (character == '_'){
+        state = 'm';
+    } else if( character >= 65 && character <= 90) {
+        // letras mayúsculas
+        state = 'm';
+    } else if( character >= 97 && character <= 122) {
+        // letras minúsculas
         state = 'm';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, variable con sintaxis incorrecta: ");
     }
     return state;
@@ -497,10 +542,15 @@ char statef_k(char character, char state) {
 char statef_l(char character, char state) {
     if (character == ' ') {
         state = 'l';
-    } else if (character == '_' || character >= 65 && character <= 90) {
+    } else if (character == '_'){
+        state = 'm';
+    } else if( character >= 65 && character <= 90) {
+        state = 'm';
+    } else if( character >= 97 && character <= 122) {
         state = 'm';
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, variable con sintaxis incorrecta: ");
     }
     return state;
@@ -508,10 +558,15 @@ char statef_l(char character, char state) {
 
 char statef_m(char character, char state) {
     if (character >= 65 && character <= 90 ||
-        character >= 97 && character <= 122 || character == '*') {
+        character >= 97 && character <= 122 ||
+        character == '*') {
         state = 'n';
+        
+    } else if(character == ';'){
+        state = 'F'; 
     } else {
         state = 'E';
+        printf("\n");
         printf("Cadena no valida, variable con caracteres invalidos: ");
     }
     return state;
@@ -526,22 +581,28 @@ char statef_n(char character, char state) {
         state = 'n';
     } else {
         state = 'E';
+        printf("\n");
         printf("No se cerro correctamente la instrucción: ");
     }
     return state;
 }
 
 char statef_final(char character, char state) {
-    if (character == '\0' || character == '\n') {
+    if (character == '\0' || character == '\n' ||
+        character == '\r') {
         state = 'F';
-    } else {
+    } else if (character == ' '){
+        printf("ADV: Espacio en blanco al final de la línea.\n");
+        state = 'F';
+    }else {
         state = 'E';
+        printf("\n");
         printf("No se llego al estado final, error de sintaxis: ");
     }
     return state;
 }
 
-char automata_funciones(char *string) {
+char automata_funciones(char *string, int number) {
     
     int i = 0;
     int iter = 0;
@@ -549,7 +610,7 @@ char automata_funciones(char *string) {
     char state = '0';
     char character = string[i];
     
-    printf("Entrada: ");
+    printf("Línea %d: ", number);
     print_string(string);
     printf("\n");
     
@@ -585,7 +646,6 @@ char automata_funciones(char *string) {
         case '9':
             state = statef_9(character, state);
             break;
-            
             // estado 10
         case 'a':
             state = statef_a(character, state);
@@ -606,8 +666,6 @@ char automata_funciones(char *string) {
         case 'e':
             state = statef_e(character, state);
             break;
-            // Aqui ingresar los otros, de g a n
-            
             // estado 15
         case 'g':
             state = statef_g(character, state);
@@ -647,6 +705,9 @@ char automata_funciones(char *string) {
         }
         
         print_state(string, i, state);
+        /* if(state == 'E' || state == 'F'){ */
+        /*     print_state(string, i, state); */
+        /* } */
         
         if (state == 'E') {
             break;
@@ -655,13 +716,9 @@ char automata_funciones(char *string) {
         character = string[i];
     }
     
-    if (state == 'E') {
-        state = i + 48;
-    }
-    
-    if (character == '\0' && state != 'F') {
-        state = '.';
-    }
+    /* if (state == 'E') { */
+    /*     state = i + 48; */
+    /* } */
     
     return state;
 }
@@ -679,8 +736,12 @@ int main(int argc, char *argv[]) {
     char *string = argv[1];
     // -----------------------
     
+    // variables del programa
     char state = '0'; // 48
-    int exit = 0;
+    int exit = 1;
+    char prev_state = '0';
+    int number_line = 0;
+    // -----------------------
     
     if (argc == 2) {
         f = fopen(argv[1], "r");
@@ -692,13 +753,29 @@ int main(int argc, char *argv[]) {
         else {
             while ((read = getline(&line, &len, f)) != -1) {
                 printf("------------\n");
+                number_line = number_line + 1;
                 char character = line[0];
-                if (character == '/') {
-                    state = automata_comentarios(line);
+                // Puede haber una c, pero esa línea puede estar
+                // dentro de un comentario
+                if (character == 'c' && state != '5') {
+                    state = automata_funciones(line, number_line);
                 }
-                if (character == 'c') {
-                    state = automata_funciones(line);
+                // líneas vacías
+                else if(character == '\n' || character == '\r'){
+                    printf("Línea vacía.\n");
                 }
+                // Se dejó al final principalmente porque los
+                // comentarios multilínea pueden empezar con
+                // cualquier carácter
+                else{
+                    prev_state = state;
+                    state = automata_comentarios(line, number_line, prev_state);
+                    printf("s = %c\n", state);
+                }
+                if(state == 'E') break;
+            }
+            if(state == 'F'){
+                exit = 0;
             }
         }
     }
